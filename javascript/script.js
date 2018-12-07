8f9fc96e9f3422904fbe965a319bec649af3122b
MarinaBoudin <marina.boudin@etu.u_bordeaux.fr>
2018-12-07 04:42:17 +0100

    additions

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+var good_date = function(){
+  var gif = document.getElementById("gif");
+  gif.src = "otter.gif";
+}
+
+var setuplisteners = function(){
+  var date = document.getElementById("date");
+  date.addEventListener("click",good_date);
+}
+
+window.addEventListener("load",setuplisteners);
