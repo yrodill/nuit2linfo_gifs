3d02df3cccb993770325ef32b7c4c44649ca92c8
yrodill <benbotho@gmail.com>
2018-12-07 05:18:53 +0100

    mouseover event

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,16 @@ var good_date = function(){
   gif.src = "../IMG/otter.gif";
 }
 
+var reset_gif = function(){
+  var gif = document.getElementById("gif");
+  gif.src = "../IMG/fevrier.gif";
+}
+
 var setuplisteners = function(){
   var date = document.getElementById("date");
+  var header = document.getElementById("fev");
   date.addEventListener("click",good_date);
+  header.addEventListener("mouseover",reset_gif);
 }
 
 window.addEventListener("load",setuplisteners);
